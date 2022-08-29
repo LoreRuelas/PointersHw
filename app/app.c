0ff347fd4863031cb7237722e5a5da752a9bce60
Your Name <you@example.com>
2022-08-28 19:54:05 -0500

    Homework finished

--- app/app.c
+++ app/app.c
@@ -1,4 +1,16 @@
+# include "pointers.h"
+
 int main(void)
 {
+
+    int collection[] = {3,4,3,3,3};
+    int count = 5;
+
+    bubble_sort(collection, count);
+    insertion_sort(collection, count);
+    reverse(collection, count);
+    calulate_frequency(collection, count, 4);
+
+
   return 0;
 }
--- lib/pointers.c
+++ lib/pointers.c
@@ -5,23 +5,80 @@
 
 #include "pointers.h"
 
-int bubble_sort(int *target, int count)
+int bubble_sort(int *collection, int count)
 {
-  // aqui su implementación.
+    int swap = 0;
+    int *prt_a_swap = &swap;
+
+
+    for(int i = 0; i < count; i++){
+        int *ptrBase = collection;
+        int *ptrNext = collection;
+        ptrNext++;
+        for(int j = 0; j < count-1; j++){
+            if(*ptrBase > *ptrNext ){
+                *prt_a_swap = *ptrBase;
+                *ptrBase = *ptrNext;
+                *ptrNext = *prt_a_swap;
+            }
+            *ptrBase++;
+            *ptrNext++;
+        }
+    }
   return 0;
 }
 
 void insertion_sort(int *collection, int count)
 {
+    int size = count;
+    for (int actual = 1; actual < size; actual++){
+        int key = (*(collection + actual));
+        int previous = actual - 1;
+        while (previous >= 0 && (*(collection + previous)) > key){
+            // hago espacio para meter key - RECORRE TODOS LOS DEMÁS ELEMENTOS
+            (*(collection + previous + 1)) = (*(collection + previous));
+            // disminuyo uno
+            previous = previous - 1;
+        }
+        // mete en la posición la key que tenemos
+        (*(collection + previous + 1)) = key;
+    }
 
 }
 
 void reverse(int *collection, int count)
 {
+    int reps;
+    int *ptr_last = (collection + (count-1));
+    int swap = 0;
+    int *ptr_swap = &swap;
+    int *ptr_init = collection;
+
+    if (count%2 == 0){
+        reps = count/2;
+    } else{
+        reps = (count-1)/2;
+    }
+
+    for(int i = 0; i < reps; i++){
+        *ptr_swap = *ptr_init;
+        *ptr_init = *ptr_last;
+        *ptr_last = *ptr_swap;
+        *ptr_last--;
+        *ptr_init++;
+    }
 
 }
 
 int calulate_frequency(int *collection, int count, int target)
 {
+    int reps = 0;
+    int *ptr_to_num = collection;
+    for(int i = 0; i < count; i++){
+        if (*ptr_to_num == target){
+            reps++;
+        }
+        *ptr_to_num++;
+    }
   return 0;
 }
